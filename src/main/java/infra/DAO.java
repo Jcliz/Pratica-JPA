3396592d9eddd0093b5812fecd0ff4736f714185
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-02-17 09:28:53 -0300

    Named native query com result set

--- src/main/java/infra/DAO.java
+++ src/main/java/infra/DAO.java
@@ -81,6 +81,11 @@ public class DAO<E> {
         return query.getResultList();
     }
 
+    public E consultarUm(String nomeConsulta, Object... params) {
+        List<E> lista = consultar(nomeConsulta, params);
+        return lista.isEmpty() ? null : lista.getFirst();
+    }
+
     public void fechar() {
         em.close();
     }
